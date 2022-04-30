3344a9039592bc248878f42f5dc034cfd81a3ca9
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2022-04-29 20:25:42 -0700

    added few lines

--- StackOverflow/Help.sql
+++ StackOverflow/Help.sql
@@ -14,4 +14,15 @@ https://sqlserverfast.com/epr/operator-list/
 https://www.brentozar.com/archive/2018/01/updated-and-smaller-stack-overflow-demo-databases/
 https://www.brentozar.com/archive/2015/10/how-to-download-the-stack-overflow-database-via-bittorrent/
 
--- 06) 
+-- 06) Using Stack Overflow Queries to Generate Workloads
+https://brentozar.com/go/workload
+
+-- 07) SQLQueryStress
+https://github.com/ErikEJ/SqlQueryStress/releases
+
+-- 08) Brent Workload Queries
+https://gist.githubusercontent.com/BrentOzar/12b8ac33a67f02f413d30529caff5676/raw/89eb4d047889c295b8f2f46ea8c08df08bb3496a/TopQueries.sql
+
+-- 09) How to Test Your Storage with CrystalDiskMark
+https://www.brentozar.com/archive/2012/03/how-fast-your-san-or-how-slow/
+
