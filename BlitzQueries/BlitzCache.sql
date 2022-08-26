412a3ef7741ee13d081e9b0786c7e49eb57c004f
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2022-08-26 15:59:00 +0530

    Added BlitzCache capturing

--- BlitzQueries/BlitzCache.sql
+++ BlitzQueries/BlitzCache.sql
@@ -2,12 +2,18 @@ EXEC tempdb..sp_BlitzCache @Help = 1
 EXEC tempdb..sp_BlitzCache @ExpertMode = 1, @ExportToExcel = 1
 
 --	https://github.com/BrentOzarULTD/SQL-Server-First-Responder-Kit#common-sp_blitzcache-parameters
-EXEC master..sp_BlitzCache @Top = 20, @SortOrder = 'reads' -- queries doing lot of reads
-EXEC master..sp_BlitzCache @Top = 20, @SortOrder = 'CPU' -- queries consuming high cpu time
-EXEC master..sp_BlitzCache @Top = 20, @SortOrder = 'writes' -- queries with high writes
-EXEC master..sp_BlitzCache @Top = 20, @SortOrder = 'memory grant' -- Queries using maximum memory grant
-exec master..sp_BlitzCache @SortOrder = 'spills', @Top = 20 -- Queries spilling to disk
-exec master..sp_BlitzCache @SortOrder = 'unused grant', @Top = 20 -- Queries asking for huge memory grant, but simply wasting it.
+EXEC master..sp_BlitzCache @Top = 30, @SortOrder = 'reads' -- queries doing lot of reads
+EXEC master..sp_BlitzCache @Top = 30, @SortOrder = 'CPU' -- queries consuming high cpu time
+EXEC master..sp_BlitzCache @Top = 30, @SortOrder = 'writes' -- queries with high writes
+EXEC master..sp_BlitzCache @Top = 30, @SortOrder = 'memory grant' -- Queries using maximum memory grant
+exec master..sp_BlitzCache @SortOrder = 'spills', @Top = 30 -- Queries spilling to disk
+exec master..sp_BlitzCache @SortOrder = 'unused grant', @Top = 30 -- Queries asking for huge memory grant, but simply wasting it.
+exec master..sp_BlitzCache @SortOrder = 'xpm', @Top = 30 -- Queries with High executions
+
+EXEC master..sp_BlitzCache @Top = 30, @SortOrder = 'reads', @IgnoreSystemDBs = 0,
+					@OutputDatabaseName = 'DBA_Admin', @OutputSchemaName = 'dbo', @OutputTableName = 'BlitzCache'
+
+select top 500 * from DBA_Admin.dbo.BlitzCache
 
 --	Analyze using Procedure Name
 exec master..sp_BlitzCache @StoredProcName = 'usp_rm_get_source_logo_gaps_Ajay'
