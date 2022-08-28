496b90a8dc9dab7a28872648b5e3b46f97625869
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2022-08-28 23:06:08 +0530

    removed one project

--- StackExchange Data Dump/Help URLs.sql
+++
@@ -1,8 +0,0 @@
---	 Database Schema Documentation
-https://meta.stackexchange.com/questions/2677/database-schema-documentation-for-the-public-data-dump-and-sede
-
---	A Presenter’s Guide to the Stack Overflow Database
-https://www.brentozar.com/archive/2018/08/a-presenters-guide-to-the-stack-overflow-database/
-
---	Dump path
-https://archive.org/details/stackexchange
