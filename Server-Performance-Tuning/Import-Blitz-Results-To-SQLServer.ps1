6c5b1910252a616e5b9452cea74c10ae9676db12
Ajay Kumar Dwivedi <ajay.dwivedi2007@gmail.com>
2022-08-31 21:44:16 +0530

    Added sql notebooks for PT

---
+++ Server-Performance-Tuning/Import-Blitz-Results-To-SQLServer.ps1
@@ -0,0 +1,8 @@
+﻿#$personal = Get-Credential -UserName 'sa' -Message 'Personal'
+$Server = 'SqlPractice'
+$IndexSummaryFile = '\\SomePath\sp_BlitzIndex-Summary-Wed-Aug31.xlsx'
+$IndexDetailedFile = '\\SomePath\sp_BlitzIndex-Detailed-Wed-Aug31.xlsx'
+
+Import-Excel $IndexSummaryFile | Write-DbaDbTableData -SqlInstance $Server -Database 'DBA_Admin' -Table 'BlitzIndex_Summary_Aug31' -SqlCredential $personal -AutoCreateTable
+Import-Excel $IndexDetailedFile | Write-DbaDbTableData -SqlInstance $Server -Database 'DBA_Admin' -Table 'BlitzIndex_Detailed_Aug31' -SqlCredential $personal -AutoCreateTable
+
