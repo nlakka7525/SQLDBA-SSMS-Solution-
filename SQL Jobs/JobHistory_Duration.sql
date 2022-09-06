73b61aa934601603245f0c3f9ce67ff516acf6c5
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2022-09-06 18:21:25 +0530

    updated infra queries

--- SQL Jobs/JobHistory_Duration.sql
+++ SQL Jobs/JobHistory_Duration.sql
@@ -19,3 +19,18 @@ and msdb.dbo.agent_datetime(run_date, run_time)
 BETWEEN '12/08/2012' and '12/10/2012'  --Uncomment for date range queries
 */
 order by RunDateTime desc,JobName
+go
+
+
+select	j.name as 'JobName',
+		msdb.dbo.agent_datetime(run_date, run_time) as 'RunDateTime',
+		((run_duration/10000*3600 + (run_duration/100)%100*60 + run_duration%100 + 31 ) / 60) as 'RunDurationMinutes',
+		h.step_id
+From msdb.dbo.sysjobs j 
+INNER JOIN msdb.dbo.sysjobhistory h 
+ ON j.job_id = h.job_id 
+where j.enabled = 1  --Only Enabled Jobs
+and msdb.dbo.agent_datetime(run_date, run_time) >= DATEADD(hour,-3,getdate())
+and j.name = '(dba) Collect-PerfmonData'
+and h.step_id = 0
+order by JobName, RunDateTime desc
