20e16d9d15f98d44e017b9048e66151e8c42c484
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2022-09-15 15:03:26 +0530

    Added PlanCacheAutoPilot

--- BlitzQueries/QRY-sp_WhoIsActive-Usages.sql
+++ BlitzQueries/QRY-sp_WhoIsActive-Usages.sql
@@ -1,9 +1,9 @@
-EXEC sp_WhoIsActive @get_outer_command = 1, @get_task_info=2 --,@get_avg_time=1,
+EXEC sp_WhoIsActive @get_outer_command = 1, @get_task_info=2, @get_additional_info=1 --,@get_avg_time=1,
 					--,@find_block_leaders=1
-					--,@get_transaction_info=1 , @get_additional_info=1	
+					--,@get_transaction_info=1
 					--,@get_full_inner_text=1
 					--,@get_locks=1
-					,@get_plans=1
+					--,@get_plans=1
 					--,@sort_order = '[CPU] DESC'
 					--,@filter = 315
 					,@filter_type = 'login' ,@filter = 'E84947'
