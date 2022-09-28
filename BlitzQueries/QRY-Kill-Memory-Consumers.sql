f7a1c66779bfbeb8e69e932590908337f287ca6e
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2022-09-28 13:16:09 +0530

    Updated memory consumers query

--- BlitzQueries/QRY-Kill-Memory-Consumers.sql
+++ BlitzQueries/QRY-Kill-Memory-Consumers.sql
@@ -3,17 +3,23 @@ declare @start_time datetime2 = sysdatetime();
 declare @my_login varchar(100) = ORIGINAL_LOGIN();
 declare @kill_string nvarchar(200);
 declare @memory_threshold_mb int = 500;
-declare @include_open_tran bit = 0;
+declare @include_open_tran bit = 1;
 
-select [memory_gb] = der.granted_query_memory*8.0/1024/1024, 
+declare @sql nvarchar(max);
+declare @params nvarchar(2000);
+set @params = N'@memory_threshold_mb int, @include_open_tran bit, @my_login varchar(100)';
+
+set quoted_identifier off;
+set @sql = "
+select [memory_gb] = convert(numeric(20,2),der.granted_query_memory*8.0/1024/1024), 
 		[--kill query--] = 'kill '+convert(varchar,der.session_id)+char(10)
 		,[--find-query--] = 'dbcc inputbuffer('+convert(varchar,der.session_id)+')'
-		,[session_tran_count] = des.open_transaction_count 
-		,[request_tran_count] = der.open_transaction_count
+		,[tran count(ssn+req)] = cast(des.open_transaction_count as varchar)+ '+' + convert(varchar,der.open_transaction_count)
 		,[elapsed_time] = convert(varchar,getdate()-der.start_time,108)
 		,der.session_id, der.status, der.command
-		,[db_name] = db_name(der.database_id)
-		,der.blocking_session_id, der.wait_type, [wait_time] = dateadd(ms,wait_time,'1900-01-01 00:00:00.000')
+		,[db_name] = db_name(der.database_id), des.login_name, des.program_name, des.host_name
+		,der.blocking_session_id, der.wait_type, 
+		[wait_time] = convert(varchar,dateadd(ms,wait_time,'1900-01-01 00:00:00.000'),108)
 		,der.wait_resource, der.percent_complete, der.cpu_time, der.total_elapsed_time
 		,der.logical_reads, der.writes, der.row_count, query_hash /* , dop, parallel_worker_count */
 from sys.dm_exec_requests der join sys.dm_exec_sessions des
@@ -24,8 +30,12 @@ and (	@include_open_tran = 1
 	)
 --and des.login_name <> @my_login
 order by granted_query_memory desc;	
-*/
+"
+set quoted_identifier on;
 
+exec sp_executesql @sql, @params, @memory_threshold_mb, @include_open_tran, @my_login;
+*/
+go
 -- Parameters
 declare @include_open_tran bit = 0,
 		@skip_my_login bit = 0,
@@ -38,7 +48,8 @@ declare @start_time datetime2 = sysdatetime(),
 		@session_id int,
 		@memory_grant_mb int;
 
-while (dateadd(MINUTE,30,@start_time) >= SYSDATETIME() )
+--while (dateadd(MINUTE,30,@start_time) >= SYSDATETIME())
+while (dateadd(MINUTE,10,@start_time) >= SYSDATETIME() and (select cntr_value from sys.dm_os_performance_counters pc where pc.counter_name like 'Memory Grants Pending%') > 0 )
 begin
 	declare cur_connections cursor static local for 
 					select [--kill query--] = 'kill '+convert(varchar,der.session_id), der.session_id,
@@ -84,5 +95,4 @@ begin
 		waitfor delay '00:01:00'
 	else
 		waitfor delay '00:00:02'
-end
-go
+end;
