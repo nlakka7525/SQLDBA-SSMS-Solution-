a17661c40e559e39c2a6db442caf02745e1675a3
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2022-10-17 21:19:42 +0530

    Updated trace flag file

--- BlitzQueries/Trace-Flags-2-Use.sql
+++ BlitzQueries/Trace-Flags-2-Use.sql
@@ -12,13 +12,16 @@ Negative
 Neutral
 ------------
 4199 -> Enable Query Optimizer Fixes
+	--
 9481 -> Old CE (pre-2014) regardless of compat level
+2312 -> Sets the query optimizer cardinality estimation model to the SQL Server 2014 (12.x) and later versions, dependent of the compatibility level of the database.
+	--
 11064 -> memory balancing for columnstore inserted
 9398 -> Disables Adaptive Joins
 7412 -> Get live execution plan
 272 -> Disables identity pre-allocation to avoid gaps in the values of an identity column in cases where the server restarts
 610 -> (Pre-2016) Enables minimal logging for indexed tables
-2312 -> Sets the query optimizer cardinality estimation model to the SQL Server 2014 (12.x) and later versions, dependent of the compatibility level of the database.
+
 
 
 Query Performance
