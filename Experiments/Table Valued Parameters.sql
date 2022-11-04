1601193d169e57daf0a14db0d3e602d89f683e80
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2022-11-04 22:19:11 +0530

    Updated TVP Script

--- Experiments/Table Valued Parameters.sql
+++ Experiments/Table Valued Parameters.sql
@@ -2,7 +2,8 @@ use StackOverflow
 go
 /*	https://learn.microsoft.com/en-us/sql/relational-databases/tables/use-table-valued-parameters-database-engine?view=sql-server-ver16
 
-	ISSUE -> SQL Server does not maintain statistics on columns of table-valued parameters.
+	ISSUE -> SQL Server does not maintain statistics on columns of table-valued parameters. So there is high chance of Cardinality Estimation issues.
+	ISSUE -> Queries using table valued parameters don't get Parallel plans
 
 https://learn.microsoft.com/en-us/sql/relational-databases/tables/use-table-valued-parameters-database-engine?view=sql-server-ver16#:~:text=SQL%20Server%20does%20not%20maintain%20statistics%20on%20columns%20of%20table%2Dvalued%20parameters.
 */
@@ -57,9 +58,11 @@ go
 declare @UserTable as StackUser;
 
 insert @UserTable 
-values (1),(4449743),(26837),(545629),(61305),(440595),(4197),(17174);
+--values (1)
+--,(4449743),(26837),(545629),(61305),(440595),(4197),(17174);
+select top 100000 Id from dbo.Users
 
-exec dbo.usp_GetUsersPosts_TVP @UserTable;
+exec dbo.usp_GetUsersPosts_TVP @UserTable with recompile
 go
 
 print '*************************************************************************'
@@ -68,7 +71,9 @@ go
 declare @UserTable as StackUser;
 
 insert @UserTable 
-values (1),(4449743),(26837),(545629),(61305),(440595),(4197),(17174);
+--values (1)
+--,(4449743),(26837),(545629),(61305),(440595),(4197),(17174);
+select top 100000 Id from dbo.Users
 
 exec dbo.usp_GetUsersPosts_TVP_TempTable @UserTable;
 go
