2e98d420729f30af8c3c6c0b413127da778d6930
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2022-12-15 14:23:02 +0530

    updated RandomQ generator code

--- StackOverflow/Invoke-RandomQ.ps1
+++ StackOverflow/Invoke-RandomQ.ps1
@@ -1,7 +1,7 @@
 ﻿[CmdletBinding()]
 Param (
     [Parameter(Mandatory=$false)]
-    [int]$NoOfIterations = 50,
+    [int]$NoOfIterations = 500,
     [Parameter(Mandatory=$false)]
     [int]$NoOfThreads = 6,
     [Parameter(Mandatory=$false)]
@@ -9,7 +9,7 @@ Param (
     [Parameter(Mandatory=$false)]
     [string]$Database = 'StackOverflow',
     [Parameter(Mandatory=$false)]
-    [int]$DelayBetweenQueriesMS = 1000,
+    [int]$DelayBetweenQueriesMS = 100,
     [Parameter(Mandatory=$false)]
     [pscredential]$SqlCredential
 )
@@ -29,12 +29,13 @@ $scriptBlock = {
     $id3 = Get-Random -Maximum 10000000
 
     # Set application/program name
-    $appName = switch ($id1 % 5) {
+    $appName = switch ($id1 % 6) {
         0 {"SQLQueryStress"}
         1 {"dbatools"}
         2 {"VS Code"}
         3 {"PowerShell"}
         4 {"Azure Data Studio"}
+        5 {"SQLQueryStress"}
     }
 
     # Randonly call b/w 2 logins
@@ -46,54 +47,57 @@ $scriptBlock = {
     }
 
     # Call various procedures
-    if (($id1 % 12) -eq 0) { # 1/12 chance
-        $r = Invoke-DbaQuery -SqlInstance $con  -Query usp_Q3160 -SqlParameter @{ UserId = $id1 } -CommandType StoredProcedure
+    if (($id1 % 30) -eq 0) { # 1/30 chance
+        $r = Invoke-DbaQuery -SqlInstance $con -Query "select top 250 * from dbo.Posts order by LastEditDate desc;" -ErrorAction Stop
+    }
+    elseif (($id1 % 12) -eq 0) { # 1/12 chance
+        $r = Invoke-DbaQuery -SqlInstance $con  -Query usp_Q3160 -SqlParameter @{ UserId = $id1 } -CommandType StoredProcedure -ErrorAction Stop
     }
     <#
-    elseif (($id1 % 12) -eq 1) { # 1/10 chance
-        $r = Invoke-DbaQuery -SqlInstance $con  -Query usp_Bounties_and_Questions_by_Month -CommandType StoredProcedure
+    elseif (($id1 % 11) -eq 1) { # 1/10 chance
+        $r = Invoke-DbaQuery -SqlInstance $con  -Query usp_Bounties_and_Questions_by_Month -CommandType StoredProcedure -ErrorAction Stop
     } #>
-    elseif (($id1 % 11) -eq 0) { # 1/11 chance
-        $r = Invoke-DbaQuery -SqlInstance $con -Query "EXEC usp_Q36660 $id1;"
-    }
     elseif (($id1 % 10) -eq 0) { # 1/10 chance
-        $r = Invoke-DbaQuery -SqlInstance $con  -Query usp_Q466 -SqlParameter @{ UserId = $id1 } -CommandType StoredProcedure
+        $r = Invoke-DbaQuery -SqlInstance $con -Query "EXEC usp_Q36660 $id1;" -ErrorAction Stop
     }
-    <#
-    elseif (($id1 % 9) -eq 0) {
-        $r = Invoke-DbaQuery -SqlInstance $con -Query "EXEC usp_Q6627 $id1;"
+    elseif (($id1 % 9) -eq 0) { # 1/9 chance
+        $r = Invoke-DbaQuery -SqlInstance $con  -Query usp_Q466 -SqlParameter @{ Useless = $id1 } -CommandType StoredProcedure -ErrorAction Stop
     }
-    #>
     elseif (($id1 % 8) -eq 0) {
-        $r = Invoke-DbaQuery -SqlInstance $con  -Query usp_Q6772 -SqlParameter @{ UserId = $id1 } -CommandType StoredProcedure
+        $r = Invoke-DbaQuery -SqlInstance $con  -Query usp_Q6772 -SqlParameter @{ UserId = $id1 } -CommandType StoredProcedure -ErrorAction Stop
     }
     elseif (($id1 % 7) -eq 0) {
-        $r = Invoke-DbaQuery -SqlInstance $con -Query "EXEC usp_Q6856 $id1;"
+        $r = Invoke-DbaQuery -SqlInstance $con -Query "EXEC usp_Q6856 $id1;" -ErrorAction Stop
     }
     elseif (($id1 % 6) -eq 0) {
-        $r = Invoke-DbaQuery -SqlInstance $con  -Query usp_Q7521 -SqlParameter @{ UserId = $id1 } -CommandType StoredProcedure
+        $r = Invoke-DbaQuery -SqlInstance $con  -Query usp_Q7521 -SqlParameter @{ UserId = $id1 } -CommandType StoredProcedure -ErrorAction Stop
     }
     elseif (($id1 % 5) -eq 0) {
-        $r = Invoke-DbaQuery -SqlInstance $con -Query "EXEC usp_Q8116 $id1;"
+        $r = Invoke-DbaQuery -SqlInstance $con -Query "EXEC usp_Q8116 $id1;" -ErrorAction Stop
     }
+    <#
+    elseif (($id2 % 4) -eq 0) { # 1/4 chance
+        $r = Invoke-DbaQuery -SqlInstance $con  -Query usp_Q466 -SqlParameter @{ Useless = $id2 } -CommandType StoredProcedure -ErrorAction Stop
+    }
+    #>
     elseif (($id1 % 4) -eq 0) {
-        $r = Invoke-DbaQuery -SqlInstance $con  -Query usp_Q947 -SqlParameter @{ UserId = $id1 } -CommandType StoredProcedure
+        $r = Invoke-DbaQuery -SqlInstance $con  -Query usp_Q947 -SqlParameter @{ UserId = $id1 } -CommandType StoredProcedure -ErrorAction Stop
     }
     elseif (($id1 % 3) -eq 0) {
-        $r = Invoke-DbaQuery -SqlInstance $con -Query "EXEC usp_Q949 $id1;"
+        $r = Invoke-DbaQuery -SqlInstance $con -Query "EXEC usp_Q949 $id1;" -ErrorAction Stop
     }
     elseif (($id1 % 2) -eq 0) {
-        $r = Invoke-DbaQuery -SqlInstance $con  -Query usp_Q952 -SqlParameter @{ UserId = $id1 } -CommandType StoredProcedure
+        $r = Invoke-DbaQuery -SqlInstance $con  -Query usp_Q952 -SqlParameter @{ Useless = $id1 } -CommandType StoredProcedure -ErrorAction Stop
     }
     <#
     elseif (($id1 % 3) -eq 1) {
-        $r = Invoke-DbaQuery -SqlInstance $con  -Query usp_get_user_comment_score_distribution -SqlParameter @{ UserId = $id1 } -CommandType StoredProcedure
+        $r = Invoke-DbaQuery -SqlInstance $con  -Query usp_get_user_comment_score_distribution -SqlParameter @{ UserId = $id1 } -CommandType StoredProcedure -ErrorAction Stop
     }
     elseif (($id1 % 3) -eq 2) {
-        $r = Invoke-DbaQuery -SqlInstance $con  -Query usp_get_user_score -SqlParameter @{ UserId = $id1 } -CommandType StoredProcedure
+        $r = Invoke-DbaQuery -SqlInstance $con  -Query usp_get_user_score -SqlParameter @{ UserId = $id1 } -CommandType StoredProcedure -ErrorAction Stop
     } #>
     else {
-        $r = Invoke-DbaQuery -SqlInstance $con -Query "EXEC usp_Q975 $id1;"
+        $r = Invoke-DbaQuery -SqlInstance $con -Query "EXEC usp_Q975;" -ErrorAction Stop
     }
 }
 $jobs = $loops | Start-RSJob -Name {"RandomQ__$_"} -ScriptBlock $scriptBlock -Throttle $NoOfThreads -ModulesToImport dbatools -ArgumentList $SqlInstance, $Database, $SqlCredential
@@ -114,16 +118,23 @@ $elapsedTime = New-TimeSpan -Start $startTime -End $endTime
 cd $env:USERPROFILE\documents\Lab-Load-Generator\
 #$SqlCredential = Get-Credential -UserName 'SQLQueryStress' -Message 'SQLQueryStress'
 
+# Get-DbaService -ComputerName SqlPractice | Restart-DbaService -Force
+# Restart-Computer -ComputerName SqlPractice -Force -Wait
+
 $params = @{
     SqlInstance = 'SqlPractice'
     Database = 'StackOverflow'
     NoOfIterations = 500
     NoOfThreads = 6
-    DelayBetweenQueriesMS = 1000
+    DelayBetweenQueriesMS = 100
     SqlCredential = $SqlCredential
 }
 
 cls
 Import-Module dbatools, PoshRSJob;
 .\Invoke-RandomQ.ps1 @params
+
+#dbcc freeproccache
+#checkpoint
+#dbcc dropcleanbuffers
 #>
